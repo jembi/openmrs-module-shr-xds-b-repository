65a5a2c11c9e69a8e9844f4773a599072e2fe386
ryan <ryan@jembi.org>
2014-08-20 15:39:15 +0200

    Adding file left out from last commit, also bumped version.

---
+++ omod/src/main/java/org/openmrs/module/xdsbrepository/ihe/iti/actors/impl/exceptions/UnsupportedGenderException.java
@@ -0,0 +1,15 @@
+package org.openmrs.module.xdsbrepository.ihe.iti.actors.impl.exceptions;
+
+public class UnsupportedGenderException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public UnsupportedGenderException(String msg) {
+		super(msg);
+	}
+
+	public UnsupportedGenderException(String msg, Throwable t) {
+		super(msg, t);
+	}
+
+}
